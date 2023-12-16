b7f86b3ee418d2160a225ba2fd58d44d5eca9d45
Dejanee Gomez <dejaneegomez@gmail.com>
2023-12-16 15:01:35 -0500

    adds parameters

---
+++ index.js
@@ -0,0 +1,20 @@
+function introduction(Samip) {
+ return `Hi, my name is ${Samip}.`;
+}
+function introductionWithLanguage(Samip, React) {
+    return `Hi, my name is ${Samip} and I am learning to program in ${React}.`;
+}
+function introductionWithLanguageOptional(Gracie, language = "JavaScript") {
+    return `Hi, my name is ${Gracie} and I am learning to program in ${"JavaScript"}.`;
+}
+function introductionWithLanguageOptional(Gracie, language = "JavaScript") {
+   return `Hi, my name is ${Gracie} and I am learning to program in ${language}.`;
+}
+
+
+
+
+
+
+
+
